ba19cb99e7bfcd2693a9d6d2b07c68304f3bb927
Magnus Borgersen <magnusborgersen@gmail.com>
2023-06-28 17:49:40 +0200

    added logo, fixed some styles, showed logged on user, readme ++

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {auth: req.isAuthenticated()});
+  res.render('index', {
+    auth: req.isAuthenticated(),
+    user: req.user?.username
+  });
 });
 
 module.exports = router;
